937c5e3ece2e9c708982a997b37fec3359d78b21
trophime1 <trophime250@gmail.com>
2024-02-19 11:01:25 +0200

    headres

--- src/pages/CreatingListing.jsx
+++ src/pages/CreatingListing.jsx
@@ -134,9 +134,6 @@ export default function CreatingListing() {
       setError(false);
       const res = await fetch(`${appUrl}/api/listing/create`, {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
         body: JSON.stringify({
           ...formData,
           userRef: currentUser._id,
